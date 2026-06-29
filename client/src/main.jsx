7ba63b5ad525ec3ffefbd674d62076f3d4d4406b
FERELKING242 <111065891+ferelking242@users.noreply.github.com>
2026-06-29 14:29:19 +0100

    chore: update client/src/main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createRoot } from 'react-dom/client';
+import './i18n/index.js';
 import './index.css';
+import App from './App.jsx';
+import { ThemeProvider } from './context/ThemeContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <ThemeProvider>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
